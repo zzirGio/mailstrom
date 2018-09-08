4f43e6543a5710462c9d740cae0c365a8700f6e7
Sergio Mesina <smes3708@uni.sydney.edu.au>
2018-09-08 20:51:28 +1000

    fix: login/registration error messages
    [#13]
    
    - Fixed Error handling

---
+++ src/main/java/au/elec5619/mailstrom/exceptions/ConflictException.java
@@ -0,0 +1,12 @@
+package au.elec5619.mailstrom.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.CONFLICT)
+public class ConflictException extends RuntimeException {
+	
+	public ConflictException(String message) {
+		super(message);
+	}
+}
