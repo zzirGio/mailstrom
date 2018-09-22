bbc79df3d1589fd4f18c937b50a586aacf0d7330
Jonathan Du <jodu8426@uni.sydney.edu.au>
2018-09-22 12:57:46 +1000

    Add login guards to message views

--- angular/src/app/app.routing.ts
+++ angular/src/app/app.routing.ts
@@ -1,32 +1,52 @@
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule } from "@angular/router";
 
 import {
-    LandingComponent,
-    LoginComponent,
-    MessageEditComponent,
-    MessageListComponent,
-    RegisterComponent,
-    DashboardComponent,
-    UserManagementComponent,
-    ResetPasswordComponent,
-    MessageCreateComponent
-} from '@views';
+  LandingComponent,
+  LoginComponent,
+  MessageEditComponent,
+  MessageListComponent,
+  RegisterComponent,
+  DashboardComponent,
+  UserManagementComponent,
+  ResetPasswordComponent,
+  MessageCreateComponent
+} from "@views";
 
-import { AuthGuard } from '@guards';
+import { AuthGuard } from "@guards";
 
 const appRoutes: Routes = [
-    { path: '', component: LandingComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
-    { path: 'reset-password', component: ResetPasswordComponent },
-    { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
-    { path: 'messages', component: MessageListComponent },
-    { path: 'create-message', component: MessageCreateComponent },
-    { path: 'edit-message/:id', component: MessageEditComponent },
-    { path: 'user-management', component: UserManagementComponent, canActivate: [AuthGuard]},
+  { path: "", component: LandingComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "reset-password", component: ResetPasswordComponent },
+  {
+    path: "dashboard",
+    component: DashboardComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "messages",
+    component: MessageListComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "create-message",
+    component: MessageCreateComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "edit-message/:id",
+    component: MessageEditComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "user-management",
+    component: UserManagementComponent,
+    canActivate: [AuthGuard]
+  },
 
-    // otherwise redirect to home
-    { path: '**', redirectTo: '' }
+  // otherwise redirect to home
+  { path: "**", redirectTo: "" }
 ];
 
 export const AppRouterModule = RouterModule.forRoot(appRoutes);
