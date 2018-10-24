734d6d20839abeaaba4094ae1c0dba02ebf23519
Jonathan Du <jodu8426@uni.sydney.edu.au>
2018-10-24 16:35:40 +1100

    Implement message list search + improve readability

--- angular/src/app/views/message-list/message-list.component.ts
+++ angular/src/app/views/message-list/message-list.component.ts
@@ -9,8 +9,10 @@ import { AlertService, MessageService } from "@app/_services";
   styleUrls: ["./message-list.component.scss"]
 })
 export class MessageListComponent implements OnInit {
+  private _searchTerm: string;
   isLoading: boolean = true;
   messages: Message[];
+  filteredMessages: Message[];
   currentUsername: string;
 
   constructor(
@@ -24,16 +26,44 @@ export class MessageListComponent implements OnInit {
     this.getMessages();
   }
 
+  get searchTerm(): string {
+    return this._searchTerm;
+  }
+
+  set searchTerm(term: string) {
+    this._searchTerm = term;
+    this.filteredMessages = this._filterMessages(term);
+  }
+
+  _messageContainsSearchTerm(message: Message, searchTerm: string): boolean {
+    const termInContact: boolean =
+      message.contact.name.search(new RegExp(searchTerm, "i")) !== -1;
+    const termInContent: boolean = 
+      message.content.search(new RegExp(searchTerm, "i")) !== -1;
+    return termInContact || termInContent;
+  }
+
+  _filterMessages(searchString: string): Message[] {
+    return this.messages.filter(message =>
+      this._messageContainsSearchTerm(message, searchString)
+    );
+  }
+
+  _sortMessagesByTimeSent(messages: Message[]): Message[] {
+    return messages
+      .filter(d => !d.isSent)
+      .sort(
+        (a, b) =>
+          new Date(a.timeToBeSent).getTime() -
+          new Date(b.timeToBeSent).getTime()
+      );
+  }
+
   getMessages() {
     this.messageService.getMessagesByUsername(this.currentUsername).subscribe(
       data => {
-        this.messages = data
-          .filter(d => !d.isSent)
-          .sort(
-            (a, b) =>
-              new Date(a.timeToBeSent).getTime() -
-              new Date(b.timeToBeSent).getTime()
-          );
+        this.messages = this._sortMessagesByTimeSent(data);
+        this.filteredMessages = this.messages;
         this.isLoading = false;
       },
       error => {
